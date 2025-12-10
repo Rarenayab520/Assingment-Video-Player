d43b59b6ba3a8b1432f7505e5b7d6591735a50f3
Rarenayab520 <nayabasmat121@gmail.com>
2025-12-10 14:36:01 +0100

    Splash activity Done

--- app/src/main/java/com/devsphere/assingment2/SplashActivity.kt
+++ app/src/main/java/com/devsphere/assingment2/SplashActivity.kt
@@ -1,20 +1,26 @@
 package com.devsphere.assingment2
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.devsphere.assingment2.R
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Delay 2 seconds → then open MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+
+        }, 2000) // 2000ms = 2 seconds
     }
 }
